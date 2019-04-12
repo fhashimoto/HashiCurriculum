ba8abf70d5fdc34677a5aac0c1944a784bcf98a4
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-12 17:43:50 -0300

    bottstrap
    Colocando bootstrap no arquivo

--- hashicurriculum/src/App.js
+++ hashicurriculum/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Cabecalho from './Corpo/cabecalho.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <Cabecalho nome="Felipe Hashimoto" endereco="Rua Bela Cintra, 201 - São Paulo, SP"/>
       </div>
     );
--- hashicurriculum/src/Corpo/cabecalho.js
+++ hashicurriculum/src/Corpo/cabecalho.js
@@ -1,17 +1,13 @@
 import React, {Component} from 'react';
 
 class Cabecalho extends Component {
-
-    constructor(props){
-      super(props);
-    }
   
     render(){
       return(
         <div>
-          <a className="nome">{this.props.nome}<br/></a>
-          <a className="endereco">{this.props.endereco}</a>
-          <a className=""></a>
+          <p className="nome">{this.props.nome}</p>
+          <address className="endereco">{this.props.endereco}</address>
+          <p className=""></p>
         </div>
       )
     }
